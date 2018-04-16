a71a18bcdffc722f4892e56fd39cd9b5c178462a
Jakub Gajewski <gajewski_jakub@yahoo.pl>
2018-04-16 13:15:26 +0200

    Initial commit

---
+++ src/main/java/pl/javatester/JavaTester/JavaTesterApplication.java
@@ -0,0 +1,12 @@
+package pl.javatester.JavaTester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTesterApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/javatester/JavaTester/JavaTesterApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.javatester.JavaTester;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaTesterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
