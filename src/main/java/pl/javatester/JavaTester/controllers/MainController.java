03caf1f1a5d614349d424c8b9eb129dca2921be0
Jakub Gajewski <gajewski_jakub@yahoo.pl>
2018-04-16 14:21:06 +0200

    eksperymernt

--- src/main/java/pl/javatester/JavaTester/controllers/MainController.java
+++ src/main/java/pl/javatester/JavaTester/controllers/MainController.java
@@ -1,7 +1,25 @@
 package pl.javatester.JavaTester.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.javatester.JavaTester.models.QuestionModel;
+import pl.javatester.JavaTester.models.repositories.QuestionReopository;
+
+import java.util.Optional;
+
+@Controller
 public class MainController {
-    void controll(){
-        System.out.println("DUPA");
+
+    @Autowired
+    QuestionReopository questionReopository;
+
+    @GetMapping("/")
+    @ResponseBody
+    public String costam() {
+        Optional<QuestionModel> questionModel = questionReopository.findById(1);
+        return questionModel.toString();
     }
+
 }
