28e08bb54a50056d29d625e7f7d85600f38f791c
Jakub Gajewski <gajewski_jakub@yahoo.pl>
2018-04-17 13:02:13 +0200

    eksperymernt

--- src/main/java/pl/javatester/JavaTester/controllers/MainController.java
+++ src/main/java/pl/javatester/JavaTester/controllers/MainController.java
@@ -3,11 +3,9 @@ package pl.javatester.JavaTester.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import pl.javatester.JavaTester.models.QuestionModel;
+import pl.javatester.JavaTester.models.forms.AnswerForm;
 import pl.javatester.JavaTester.models.repositories.QuestionReopository;
 import pl.javatester.JavaTester.models.repositories.UserRepository;
 
@@ -29,16 +27,28 @@ public class MainController {
 
     @GetMapping("/question")
     public String displayQuestion(Model model) {
+        model.addAttribute ("answerForm", new AnswerForm());
         model.addAttribute("questionModel", questionReopository.findById(1));
         //Optional<QuestionModel> questionModel = questionReopository.findById(1);
         return "question";
     }
+//
+//    @PostMapping("/question")
+//    public String answerQuestion(Model model) {
+//        model.addAttribute("questionModel", questionReopository.findById(1));
+//        //Optional<QuestionModel> questionModel = questionReopository.findById(1);
+//        return "question";
+//    }
 
     @PostMapping("/question")
-    public String answerQuestion(Model model) {
-        model.addAttribute("questionModel", questionReopository.findById(1));
-        //Optional<QuestionModel> questionModel = questionReopository.findById(1);
-        return "question";
+    @ResponseBody
+    public String answerQuestionTOZUPELNIENIEMAZNACZENIANIGDZIE(@ModelAttribute("answerForm") AnswerForm babababa){
+        if(questionReopository.findById(1).getCorrect() == Integer.valueOf(babababa.getCyfra())) {
+            return "DOBRZE";
+        }
+        else {
+            return "ŹLE";
+        }
     }
 }
 
